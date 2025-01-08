c5e08553e04cb54208cc15b079cf1c58b1b57c40
Brendon Dong <Brendon4496@gmail.com>
2025-01-08 15:03:55 -0500

    Basic User and Role entities created (no foreign keys yet)

--- src/main/java/com/checkinn/checkinn/Entities/Role.java
+++ src/main/java/com/checkinn/checkinn/Entities/Role.java
@@ -1,4 +1,21 @@
 package com.checkinn.checkinn.Entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Role {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "role_id", unique = true)
+    private int roleId;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+    @Column(name = "is_admin")
+    private boolean isAdmin;
 }
--- src/main/java/com/checkinn/checkinn/Entities/User.java
+++ src/main/java/com/checkinn/checkinn/Entities/User.java
@@ -1,4 +1,25 @@
 package com.checkinn.checkinn.Entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id", unique = true)
+    private int userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
 }
