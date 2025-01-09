0e034d3de7f8d7dc30ab0627eca3ab6c267d559e
neils <neilson.john38781@gmail.com>
2025-01-09 18:22:24 -0500

    added base constructor to review controller

--- src/main/java/com/checkinn/checkinn/Controllers/ReviewController.java
+++ src/main/java/com/checkinn/checkinn/Controllers/ReviewController.java
@@ -1,4 +1,20 @@
 package com.checkinn.checkinn.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.checkinn.checkinn.Services.ReviewService;
+
+@RestController
+@RequestMapping("/reviews")
 public class ReviewController {
+
+    private ReviewService reviewService;
+
+    @Autowired
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
 }
