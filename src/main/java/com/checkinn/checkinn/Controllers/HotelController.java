b5fdc291717df24bb82460674f073a0dea226413
neils <neilson.john38781@gmail.com>
2025-01-09 11:32:44 -0500

    added base endpoints for all of hotelcontroller

--- src/main/java/com/checkinn/checkinn/Controllers/HotelController.java
+++ src/main/java/com/checkinn/checkinn/Controllers/HotelController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +42,21 @@ public class HotelController {
     @GetMapping("/location/{location}")
     public ResponseEntity<Iterable<Hotel>> getHotelsByLocation(@PathVariable String location) {
         return ResponseEntity.ok().body(hotelService.getHotelsByLocation(location));
+    }
+
+    @PatchMapping("/edit/{hotel_id}")
+    public ResponseEntity<Hotel> editHotel(@PathVariable int hotel_id) {
+        return null;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel) {
+        return null;
+    }
+
+    @PostMapping("/del/{hotel_id}")
+    public ResponseEntity<Hotel> deleteHotel(@PathVariable int hotel_id) {
+        return null;
     }
 
 }
