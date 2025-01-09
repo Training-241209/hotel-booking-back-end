f35c7bea561b4e5b8e6335517fa4bb720f3c381f
neils <neilson.john38781@gmail.com>
2025-01-09 18:25:55 -0500

    created defaults in wrong files, removed now

--- src/main/java/com/checkinn/checkinn/Repositories/ReservationRepository.java
+++ src/main/java/com/checkinn/checkinn/Repositories/ReservationRepository.java
@@ -1,4 +1,8 @@
 package com.checkinn.checkinn.Repositories;
 
-public class ReservationRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import com.checkinn.checkinn.Entities.Hotel;
+
+public interface ReservationRepository extends CrudRepository<Review, Integer>{
 }
--- src/main/java/com/checkinn/checkinn/Services/ReservationService.java
+++ src/main/java/com/checkinn/checkinn/Services/ReservationService.java
@@ -1,18 +1,4 @@
 package com.checkinn.checkinn.Services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.checkinn.checkinn.Repositories.ReservationRepository;
-
-@Service
 public class ReservationService {
-
-    private ReservationRepository reservationRepository;
-
-    @Autowired
-    public ReservationService(ReservationRepository reservationRepository) {
-        this.reservationRepository = reservationRepository;
-    }
-
 }
