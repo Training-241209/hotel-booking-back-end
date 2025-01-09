667f7535a43c49b9beec7f90ef478a518d6aef53
neils <neilson.john38781@gmail.com>
2025-01-09 18:23:19 -0500

    created base review service

--- src/main/java/com/checkinn/checkinn/Services/ReservationService.java
+++ src/main/java/com/checkinn/checkinn/Services/ReservationService.java
@@ -1,4 +1,18 @@
 package com.checkinn.checkinn.Services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.checkinn.checkinn.Repositories.ReservationRepository;
+
+@Service
 public class ReservationService {
+
+    private ReservationRepository reservationRepository;
+
+    @Autowired
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
+
 }
