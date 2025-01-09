4f21ea53f5441ce5a18fd9d6e9e0644e53c16299
neils <neilson.john38781@gmail.com>
2025-01-09 18:28:30 -0500

    created default constructor in review service

--- src/main/java/com/checkinn/checkinn/Services/ReviewService.java
+++ src/main/java/com/checkinn/checkinn/Services/ReviewService.java
@@ -1,4 +1,16 @@
 package com.checkinn.checkinn.Services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReviewService {
+
+    private ReviewService reviewService;
+
+    @Autowired
+    public ReviewService(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
 }
