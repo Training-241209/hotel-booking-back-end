084d27b6d823b10147567be44d0fa85f8cef405a
neils <neilson.john38781@gmail.com>
2025-01-09 11:36:05 -0500

    merged main

--- src/main/java/com/checkinn/checkinn/Controllers/HotelController.java
+++ src/main/java/com/checkinn/checkinn/Controllers/HotelController.java
@@ -1,4 +1,62 @@
 package com.checkinn.checkinn.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.checkinn.checkinn.Entities.Hotel;
+import com.checkinn.checkinn.Services.HotelService;
+
+@RestController
+@RequestMapping("/hotels")
 public class HotelController {
+
+    private HotelService hotelService;
+    
+    @Autowired
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
+    @GetMapping("/{hotel_id}")
+    public ResponseEntity<Hotel> getHotelById(@PathVariable int hotel_id) {
+        Hotel hotel = hotelService.getHotelById(hotel_id);
+        
+        if (hotel == null) { return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); }
+        
+        return ResponseEntity.ok().body(hotel);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Iterable<Hotel>> getAllHotels() {
+        return ResponseEntity.ok().body(hotelService.getAllHotels());
+    }
+
+    @GetMapping("/location/{location}")
+    public ResponseEntity<Iterable<Hotel>> getHotelsByLocation(@PathVariable String location) {
+        return ResponseEntity.ok().body(hotelService.getHotelsByLocation(location));
+    }
+
+    @PatchMapping("/edit/{hotel_id}")
+    public ResponseEntity<Hotel> editHotel(@PathVariable int hotel_id) {
+        return null;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel) {
+        return null;
+    }
+
+    @PostMapping("/del/{hotel_id}")
+    public ResponseEntity<Hotel> deleteHotel(@PathVariable int hotel_id) {
+        return null;
+    }
+
 }
--- src/main/java/com/checkinn/checkinn/Repositories/HotelRepository.java
+++ src/main/java/com/checkinn/checkinn/Repositories/HotelRepository.java
@@ -1,4 +1,10 @@
 package com.checkinn.checkinn.Repositories;
 
-public class HotelRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import com.checkinn.checkinn.Entities.Hotel;
+
+public interface HotelRepository extends CrudRepository<Hotel, Integer>{
+
+    Iterable<Hotel> findByLocation(String location);
 }
