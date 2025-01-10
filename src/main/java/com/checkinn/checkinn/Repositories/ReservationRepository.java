c1f9812281a7f99b260041dcf4498a23f4551de8
neils <neilson.john38781@gmail.com>
2025-01-09 19:42:26 -0500

    revert of file

--- src/main/java/com/checkinn/checkinn/Repositories/ReservationRepository.java
+++ src/main/java/com/checkinn/checkinn/Repositories/ReservationRepository.java
@@ -1,8 +1,4 @@
 package com.checkinn.checkinn.Repositories;
 
-import org.springframework.data.repository.CrudRepository;
-
-import com.checkinn.checkinn.Entities.Reservation;
-
-public interface ReservationRepository extends CrudRepository<Reservation, Integer>{
+public class ReservationRepository{
 }
