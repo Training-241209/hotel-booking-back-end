00ec6ff1670afab0ed4249ac177ce1195c38a6ea
neils <neilson.john38781@gmail.com>
2025-01-11 15:46:18 -0500

    created controller skeleton

--- src/main/java/com/checkinn/checkinn/Controllers/ReservationController.java
+++ src/main/java/com/checkinn/checkinn/Controllers/ReservationController.java
@@ -1,4 +1,56 @@
 package com.checkinn.checkinn.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.checkinn.checkinn.Services.ReservationService;
+
+@Controller
+@RequestMapping("/reservations")
 public class ReservationController {
+
+    private ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+    
+    @GetMapping("/")
+    public String getAllReservations() {
+        return "reservations";
+    }
+
+    @GetMapping("/user")
+    public String getReservationsBySelf() {
+        return "reservations";
+    }
+
+    @GetMapping("/user/{userId}")
+    public String getReservationsByUserId() {
+        return "reservations";
+    }
+
+    @GetMapping("/hotel/{hotelId}")
+    public String getReservationsByHotelId() {
+        return "reservations";
+    }
+
+    @PatchMapping("/edit/{reservationId}")
+    public String editReservation() {
+        return "reservations";
+    }
+
+    @PostMapping("/create/{hotelId}")
+    public String createReservation() {
+        return "reservations";
+    }
+
+    @DeleteMapping("/delete/{reservationId}")
+    public String deleteReservation() {
+        return "reservations";
+    }
 }
