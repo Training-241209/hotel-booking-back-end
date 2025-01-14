0e3282688df57389c207a4e691fa0344cf8fd239
Brendon Dong <Brendon4496@gmail.com>
2025-01-13 19:18:04 -0500

    Bending over backwards for the first functional service tests

--- src/test/java/com/checkinn/checkinn/CheckInnApplicationTests.java
+++ src/test/java/com/checkinn/checkinn/CheckInnApplicationTests.java
@@ -2,7 +2,9 @@ package com.checkinn.checkinn;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class CheckInnApplicationTests {
 
